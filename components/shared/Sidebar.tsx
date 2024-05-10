da2b7925fbba070300a060198ecd75af0bed02fa
Archit Mahule <mahulearchit@gmail.com>
2024-05-10 22:49:38 +0530

    update logo

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -9,12 +9,16 @@ import { navLinks } from '@/contants'
 
 const Sidebar = () => {
   const pathname = usePathname();
+  const logoStyle = {
+    height: '100%',
+    width: '100%',
+  }
 
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
         <Link href="/" className="sidebar-logo">
-          <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
+          <Image src="/assets/images/luminexa-logo.png" alt="logo" width={200} height={50} style={logoStyle} />
         </Link>
 
         <nav className="sidebar-nav">
